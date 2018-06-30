cc72674f7f0cab31f3de316d3bde692b30129005
yann <yann.boisselier@gmail.com>
2018-06-30 17:41:32 +0200

    initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+  /* config options here */
+  publicRuntimeConfig: {
+    WORDPRESS_API_URL:
+      "https://public-api.wordpress.com/wp/v2/sites/reactapidemo.wordpress.com"
+  }
+};
