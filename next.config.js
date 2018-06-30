3eecc1bf004cbb898fadd8a32584aacf72be0ddd
yann <yann.boisselier@gmail.com>
2018-06-30 19:13:51 +0200

    add a service to get public config in a easiest way

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   /* config options here */
   publicRuntimeConfig: {
+    SITE_NAME: "REACTPRESS DEMO BLOG",
     WORDPRESS_API_URL:
       "https://public-api.wordpress.com/wp/v2/sites/yinrun.wordpress.com"
   }
