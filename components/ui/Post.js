a9f8988b368ec6dcefca9c75847e6a8d9a235779
yann <yann.boisselier@gmail.com>
2018-07-02 12:05:51 +0200

    remov console log, add author on Post component

--- components/ui/Post.js
+++ components/ui/Post.js
@@ -1,6 +1,12 @@
+import AuthorList from "../ui/AuthorList";
+import moment from "moment";
 const Post = ({ post }) => (
   <div>
-    <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+    <h1 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+    <h2>
+      by <AuthorList authors={post._embedded.author} /> -{" "}
+      {moment(post.date).format("LLL")}
+    </h2>
     <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
   </div>
 );
